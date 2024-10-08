1d6a24e36892e5746ddfd348024f259e6c52320e
vipa0120 <110191352+vipa0120@users.noreply.github.com>
2024-10-08 12:59:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   name = os.environ.get("Name", "World")
-  return "Hello {} WGS!".format(name)
+  return "Hello {} Worldline Global Services!".format(name)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
