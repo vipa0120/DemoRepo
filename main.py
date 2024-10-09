ca5fe52b088bda5a8227c21ece39f784aec68fdf
vipa0120 <110191352+vipa0120@users.noreply.github.com>
2024-10-09 10:59:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  name = os.environ.get("Name", "World")
+  name = os.environ.get("Name", "Worldline")
   return "Hello {} ".format(name)
 
 if __name__ == "__main__":
